116330c81dc3c8fd5e7915f30be4cc19dd2aedd6
luluochoa7 <luisochoa606@gmail.com>
2025-11-13 19:09:59 -0600

    Adding tablename to model of user

--- backend/models.py
+++ backend/models.py
@@ -13,6 +13,7 @@ class ContactSubmission(Base):
     created_at = Column(DateTime(timezone=True), default=func.now())
 
 class User(Base):
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False, index=True)
