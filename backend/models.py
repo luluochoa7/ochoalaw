af749ccba446f0a21d8a8505be65d93a0f3fb1e5
luluochoa7 <luisochoa606@gmail.com>
2025-12-11 17:30:55 -0600

    Adding one more small change to models.py

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime, func, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base     
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship     
 
 Base = declarative_base()
 
